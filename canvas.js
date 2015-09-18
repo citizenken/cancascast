2be2bcf329fa2c917a3ac623b242f1fd224efcb6
Ken Petti <ken.petti@cloudlock.com>
2015-09-17 22:21:30 -0400

    asdfasdf
    Change-Id: Ie0689c4b0e8b551ce98d31768a991fa5d7d3b000

--- canvas.js
+++ canvas.js
@@ -3,26 +3,6 @@ window.onload = function () {
     var colorPurple = "#cb3594";
     $('#canvas').mouseup(function(e){
         paint = false;
-
-    $('#canvas').mouseleave(function(e){
-        paint = false;
-    var clickX = new Array();
-    var clickY = new Array();
-    var clickDrag = new Array();
-
-    function redraw(){
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-
-      context.lineJoin = "round";
-      context.lineWidth = 25;
-      for(var i=0; i < clickX.length; i++) {
-        context.beginPath();
-        if(clickDrag[i] && i){
-          context.moveTo(clickX[i-1], clickY[i-1]);
-         }else{
-           context.moveTo(clickX[i]-1, clickY[i]);
-         }
-         context.lineTo(clickX[i], clickY[i]);
          context.closePath();
          context.strokeStyle = clickColor[i];
          context.stroke();
